305d68c4e3641008220ab82beec7eab69b67b31e
portfiend <109661617+portfiend@users.noreply.github.com>
2026-08-07 04:37:03 -0500

    use attribute syntax for subscriptions

--- Content.Shared/_MACRO/Species/Kodepiia/Consume/SharedConsumeSystem.cs
+++ Content.Shared/_MACRO/Species/Kodepiia/Consume/SharedConsumeSystem.cs
@@ -4,29 +4,24 @@ using Content.Shared.DoAfter;
 using Robust.Shared.Serialization;
 
 namespace Content.Shared._MACRO.Species.Kodepiia.Consume;
+
 /// <summary>
-/// System that handles entities that consume other entities.. It's entity cannibalism.
+///     System that handles entities that consume other entities.. It's entity cannibalism.
 /// </summary>
 public abstract partial class SharedConsumeSystem : EntitySystem
 {
-
     [Dependency] private SharedActionsSystem _actionsSystem = default!;
-    public override void Initialize()
-    {
-        base.Initialize();
 
-        SubscribeLocalEvent<ConsumeActionComponent, ComponentStartup>(OnStartup);
-        SubscribeLocalEvent<ConsumeActionComponent, ComponentShutdown>(OnShutdown);
-    }
-
-    private void OnShutdown(Entity<ConsumeActionComponent> ent, ref ComponentShutdown args)
-    {
-        _actionsSystem.RemoveAction(ent.Owner, ent.Comp.ConsumeAction);
-    }
-
+    [SubscribeLocalEvent]
     private void OnStartup(Entity<ConsumeActionComponent> ent, ref ComponentStartup args)
     {
         _actionsSystem.AddAction(ent, ref ent.Comp.ConsumeAction, ent.Comp.ConsumeActionId);
+    }
+
+    [SubscribeLocalEvent]
+    private void OnShutdown(Entity<ConsumeActionComponent> ent, ref ComponentShutdown args)
+    {
+        _actionsSystem.RemoveAction(ent.Owner, ent.Comp.ConsumeAction);
     }
 }
 
